e13518f1663b37250cf3766c2b997ce889e2f62d
Ani Krivova <anjelika.k08@gmail.com>
2015-02-18 02:22:22 +0200

    game layout

---
+++ Homework /Task 8/gamefunctionality.js
@@ -0,0 +1,41 @@
+function StartGame(){
+	var beggin= document.getElementById("starbtn");
+	beggin.style.display = 'none';
+}
+
+
+var time;
+var counter=0;
+function Timer(){
+	document.getElementById("txt").value=counter;
+	counter=counter+1;
+	time=setTimeout(function(){ Timer() },1000);
+}
+
+/*
+<script>
+var c = 0;
+var t;
+var timer_is_on = 0;
+
+function timedCount() {
+    document.getElementById("timerPlacer").value = c;
+    c = c + 1;
+    t = setTimeout(function(){ timedCount() }, 1000);
+}
+
+function startCount() {
+    if (!timer_is_on) {
+        timer_is_on = 1;
+        timedCount();
+    }
+}
+
+function stopCount() {
+    clearTimeout(t);
+    timer_is_on = 0;
+}
+</script>
+
+
+*/
